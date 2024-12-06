3c2e00d72869d79ddca375b5bd1dda592c860691
Pablo Rojas <151286066+Pablotas04@users.noreply.github.com>
2024-12-06 15:54:48 -0600

    Update BrandListPage.xaml.cs

--- BrandListPage.xaml.cs
+++ BrandListPage.xaml.cs
@@ -9,15 +9,6 @@ namespace MauiProject
         public BrandListPage()
         {
             InitializeComponent();
-
-            // Inicializar la lista de marcas
-            /*Brands = new ObservableCollection<Brand>
-            {
-                new Brand { Name = "Nike", Description = "Marca deportiva internacional" },
-                new Brand { Name = "Adidas", Description = "Competidor líder en calzado y ropa deportiva" },
-                new Brand { Name = "Puma", Description = "Innovación en deportes y estilo" },
-            };
-            */
             
             Brands = AppShell.SharedBrands;
             BindingContext = this;
